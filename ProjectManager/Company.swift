d4f8c14a53fde75100e32c3e44edc005c499d5ab
radyabo <oemerb44@gmail.com>
2016-11-02 18:46:13 -0700

    Created empty class and protocol files. Created project-structure/folders.

---
+++ ProjectManager/Company.swift
@@ -0,0 +1,13 @@
+//
+//  Company.swift
+//  ProjectManager
+//
+//  Created by Ömer Baydar on 02.11.16.
+//  Copyright © 2016 cmpe137. All rights reserved.
+//
+
+import Foundation
+
+class Company {
+    
+}
---
+++ ProjectManager/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  ProjectManager
+//
+//  Created by Ömer Baydar on 02.11.16.
+//  Copyright © 2016 cmpe137. All rights reserved.
+//
+
+import Foundation
+
+protocol Employee {
+    
+    
+}
