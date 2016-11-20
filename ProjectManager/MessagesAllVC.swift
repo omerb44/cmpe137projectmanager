c8a17de6ef8267422300d29044f772df4ef49e53
radyabo <oemerb44@gmail.com>
2016-11-20 00:39:54 -0800

    Messages All VC segue fixed

--- ProjectManager/MessagesAllVC.swift
+++ ProjectManager/MessagesAllVC.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class MessagesAllVC: UIViewController {
+class MessagesAllVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var allMessagesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        self.allMessagesTableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell = allMessagesTableView.dequeueReusableCell(withIdentifier: "testCell")!
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsets.zero
+        cell.layoutMargins = UIEdgeInsets.zero
+        return cell
     }
     
 
