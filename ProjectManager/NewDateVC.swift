77ee58058ecefb892c0e9fa50941f8ed63b9018b
radyabo <oemerb44@gmail.com>
2016-11-20 19:58:18 -0800

    NewDate VC updated

--- ProjectManager/NewDateVC.swift
+++ ProjectManager/NewDateVC.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
-class NewDateVC: UIViewController {
+class NewDateVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBAction func closeView(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var datePlace: UITextField!
+    @IBOutlet weak var dateTitel: UITextField!
+    @IBOutlet weak var dateDatePicker: UIDatePicker!
+    @IBOutlet weak var switchTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
+        self.switchTableView.tableFooterView = UIView()
+        datePlace.useUnderline()
+        dateTitel.useUnderline()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +31,30 @@ class NewDateVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        switch indexPath.row {
+        case 0:
+            cell = switchTableView.dequeueReusableCell(withIdentifier: "onlyMe")!
+            break
+        case 1:
+            cell = switchTableView.dequeueReusableCell(withIdentifier: "everyone")!
+            break
+        default: break
+        }
+
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsets.zero
+        cell.layoutMargins = UIEdgeInsets.zero
+        return cell
+    }
 
 }
